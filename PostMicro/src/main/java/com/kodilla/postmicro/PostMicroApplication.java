9b748cde13ed29714f253a5e3a4128ec948a1229
Filip Gabrys <el.filberto@gmail.com>
2022-07-08 14:44:45 +0200

    Add initial files for Post Micro Service

---
+++ PostMicro/src/main/java/com/kodilla/postmicro/PostMicroApplication.java
@@ -0,0 +1,13 @@
+package com.kodilla.postmicro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostMicroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PostMicroApplication.class, args);
+    }
+
+}
